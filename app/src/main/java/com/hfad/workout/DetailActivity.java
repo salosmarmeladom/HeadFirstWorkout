006b7cef00eb4bfd52622331dd9da7628c91c40d
salosmarmeladom <pollyklaps@outlook.com>
2022-04-09 13:36:06 +0300

    stopwatch adding

--- app/src/main/java/com/hfad/workout/DetailActivity.java
+++ app/src/main/java/com/hfad/workout/DetailActivity.java
@@ -3,6 +3,9 @@ package com.hfad.workout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_WORKOUT_ID = "id";
 
@@ -13,5 +16,23 @@ public class DetailActivity extends AppCompatActivity {
         WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
         int workoutId = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
         frag.setWorkout(workoutId);
+    }
+
+    class Solution {
+
+        public int[] twoSum(int[] nums, int target) {
+            List<Integer> numsList = null;
+            for (int x = 0; x < nums.length; x++){
+                numsList.add(nums[x]);
+            }
+            int[] twoSum = new int[2];
+            for (int x = 0; x < nums.length; x++){
+                if (numsList.contains(target-nums[x])&&(nums[x]!=target-nums[x])){
+                    twoSum[0]=x;
+                    twoSum[1]=numsList.indexOf(target-nums[x]);
+                }
+            }
+            return twoSum;
+        }
     }
 }
